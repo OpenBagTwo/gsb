67a1bd8c5149d429dcd7975f2fe388b7d2812b85
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2023-07-18 16:23:26 -0400

    Begin implementing click-based CLI, starting with backup
    Implements #3

--- gsb/test/test_backup.py
+++ gsb/test/test_backup.py
@@ -1,10 +1,36 @@
 """Tests for creating backups"""
+import subprocess
 from pathlib import Path
 
 import pygit2
 import pytest
 
 from gsb import _git, backup, onboard
+
+
+@pytest.fixture
+def repo_root(tmp_path):
+    root = tmp_path / "roto-rooter"
+
+    my_world = root / "my world"
+    my_world.mkdir(parents=True)
+
+    my_save_data = my_world / "level.dat"
+    my_save_data.write_text("Spawn Point: (0, 63, 0)\n")
+
+    onboard.create_repo(root, my_world.name, ignore=["cruft", "ignore me"])
+
+    my_save_data.write_text("Coordinates: (20, 71, -104)\n")
+
+    (my_world / "new file").write_text("Hello, I'm new.\n")
+
+    (my_world / "cruft").write_text("Boilerplate\n")
+
+    ignoreme = my_world / "ignore me"
+    ignoreme.mkdir()
+    (ignoreme / "content.txt").write_text("Shouting into the void\n")
+
+    yield root
 
 
 @pytest.mark.usefixtures("patch_tag_naming")
@@ -18,30 +44,6 @@ class TestCreateBackup:
             _git.init(random_folder)
         with pytest.raises(OSError):
             backup.create_backup(random_folder)
-
-    @pytest.fixture
-    def repo_root(self, tmp_path):
-        root = tmp_path / "roto-rooter"
-
-        my_world = root / "my world"
-        my_world.mkdir(parents=True)
-
-        my_save_data = my_world / "level.dat"
-        my_save_data.write_text("Spawn Point: (0, 63, 0)\n")
-
-        onboard.create_repo(root, my_world.name, ignore=["cruft", "ignore me"])
-
-        my_save_data.write_text("Coordinates: (20, 71, -104)\n")
-
-        (my_world / "new file").write_text("Hello, I'm new.\n")
-
-        (my_world / "cruft").write_text("Boilerplate\n")
-
-        ignoreme = my_world / "ignore me"
-        ignoreme.mkdir()
-        (ignoreme / "content.txt").write_text("Shouting into the void\n")
-
-        yield root
 
     @pytest.mark.parametrize("tagged", (True, False), ids=("tagged", "untagged"))
     def test_backup_adds_from_manifest(self, repo_root, tagged):
@@ -89,3 +91,52 @@ class TestCreateBackup:
 
         repo = _git._repo(repo_root, new=False)
         assert str(repo.revparse_single(tag_name).target) == hash
+
+
+class TestCLI:
+    @pytest.fixture
+    def prior_commits(self, repo_root):
+        yield list(_git.log(repo_root))
+
+    @pytest.fixture
+    def prior_tags(self, repo_root):
+        yield list(_git.get_tags(repo_root, False))
+
+    def test_default_options_creates_untagged_backup_from_cwd(
+        self, repo_root, prior_commits, prior_tags
+    ):
+        subprocess.run(["gsb", "backup"], cwd=repo_root)
+
+        assert (
+            len(list(_git.log(repo_root))),
+            len(list(_git.get_tags(repo_root, False))),
+        ) == (
+            len(prior_commits) + 1,
+            len(prior_tags),
+        )
+
+    @pytest.mark.parametrize("how", ("by_argument", "by_option"))
+    def test_passing_in_a_custom_root(self, repo_root, how, prior_commits):
+        args = ["gsb", "backup", repo_root]
+        if how == "by_option":
+            args.insert(2, "--path")
+
+        subprocess.run(args)
+
+        assert len(list(_git.log(repo_root))) == len(prior_commits) + 1
+
+    @pytest.mark.usefixtures("patch_tag_naming")
+    def test_creating_a_tagged_backup(self, repo_root, prior_commits, prior_tags):
+        subprocess.run(["gsb", "backup", "--tag", "Hello"], cwd=repo_root)
+
+        tags = list(_git.get_tags(repo_root, False))
+
+        assert (
+            len(list(_git.log(repo_root))),
+            len(tags),
+        ) == (
+            len(prior_commits) + 1,
+            len(prior_tags) + 1,
+        )
+
+        assert tags[-1].annotation == "Hello\n"
