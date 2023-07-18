d7ad8dcfafe3dd680096ece6d9a673b70302349b
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2023-07-18 16:23:26 -0400

    Begin implementing click-based CLI, starting with backup
    Implements #3

--- gsb/cli.py
+++ gsb/cli.py
@@ -1,16 +1,44 @@
-# Command-line interface
-from argparse import ArgumentParser
+"""Command-line interface"""
+from pathlib import Path
+
+import click
+
+from . import _version
+from . import backup as backup_
 
 
-def generate_parsers() -> tuple[ArgumentParser, dict[str, ArgumentParser]]:
-    """Generate the command-line parsers
+@click.group()
+@click.help_option("--help", "-h")
+@click.version_option(_version.get_versions()["version"], "--version", "-v", "-V")
+def main():
+    """CLI for managing incremental backups of your save states using Git!"""
+    pass
 
-    Returns
-    -------
-    gsb_parser : ArgumentParser
-        The top-level argument parser responsible for routing arguments to
-        specific action parsers
-    action_parsers : dict of str to ArgumentParser
-        The verb-specific argument parsers
-    """
-    raise NotImplementedError
+
+@main.command()
+@click.argument(
+    "path_as_arg",
+    type=Path,
+    required=False,
+    metavar="SAVE_PATH",
+)
+@click.option(
+    "--path",
+    type=Path,
+    metavar="SAVE_PATH",
+    help=(
+        "Optionally specify the root directory containing your save data."
+        " If no path is given, the current working directory will be used."
+    ),
+)
+@click.option(
+    "--tag",
+    type=str,
+    help='Specify a description for this backup and "tag" it for future reference.',
+)
+def backup(path_as_arg: Path | None, path: Path | None, tag: str | None):
+    """Create a new backup"""
+    if path_as_arg is not None and path is not None and path_as_arg != path:
+        raise SyntaxError("Conflicting values given for SAVE_PATH")
+    repo_root = path_as_arg or path or Path()
+    backup_.create_backup(repo_root, tag)
