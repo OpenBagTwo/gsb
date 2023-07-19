91fad9c7a9669fc737d4ad8cede8c9cf834e4fd2
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2023-07-19 08:46:21 -0400

    Refactor CLI boilerplate into a single decorator

--- gsb/cli.py
+++ gsb/cli.py
@@ -1,5 +1,7 @@
 """Command-line interface"""
+import functools
 from pathlib import Path
+from typing import Callable
 
 import click
 
@@ -15,30 +17,44 @@ def gsb():
     pass
 
 
-@gsb.command()
-@click.argument(
-    "path_as_arg",
-    type=Path,
-    required=False,
-    metavar="SAVE_PATH",
-)
-@click.option(
-    "--path",
-    type=Path,
-    metavar="SAVE_PATH",
-    help=(
-        "Optionally specify the root directory containing your save data."
-        " If no path is given, the current working directory will be used."
-    ),
-)
+def _subcommand_init(command: Callable) -> Callable:
+    """Register a subcommand and add some standard CLI handling"""
+
+    @functools.wraps(command)
+    def wrapped(
+        path_as_arg: Path | None, path: Path | None, *args, **kwargs
+    ) -> Callable:
+        if path_as_arg is not None and path is not None and path_as_arg != path:
+            raise SyntaxError("Conflicting values given for SAVE_PATH")
+        repo_root = path_as_arg or path or Path()
+        return command(repo_root, *args, **kwargs)
+
+    wrapped = click.argument(
+        "path_as_arg",
+        type=Path,
+        required=False,
+        metavar="SAVE_PATH",
+    )(wrapped)
+    wrapped = click.option(
+        "--path",
+        type=Path,
+        metavar="SAVE_PATH",
+        help=(
+            "Optionally specify the root directory containing your save data."
+            " If no path is given, the current working directory will be used."
+        ),
+    )(wrapped)
+
+    return gsb.command()(wrapped)
+
+
 @click.option(
     "--tag",
     type=str,
     help='Specify a description for this backup and "tag" it for future reference.',
 )
-def backup(path_as_arg: Path | None, path: Path | None, tag: str | None):
+@_subcommand_init
+def backup(repo_root: Path, tag: str | None):
     """Create a new backup"""
-    if path_as_arg is not None and path is not None and path_as_arg != path:
-        raise SyntaxError("Conflicting values given for SAVE_PATH")
-    repo_root = path_as_arg or path or Path()
+
     backup_.create_backup(repo_root, tag)
