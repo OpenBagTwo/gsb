6f45a3ac5a2629e9322b08c254c8cd6d0f45fd02
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2023-07-19 09:59:38 -0400

    Implement gsb init CLI
    Resolves #1
    
    So click intentionally disallows variable-length options because it would be ambiguous, especially with variable-length arguments. See: https://github.com/pallets/click/issues/484#issuecomment-192257806
    
    Makes sense.

--- gsb/cli.py
+++ gsb/cli.py
@@ -7,6 +7,7 @@ import click
 
 from . import _version
 from . import backup as backup_
+from . import onboard
 
 
 @click.group()
@@ -21,20 +22,9 @@ def _subcommand_init(command: Callable) -> Callable:
     """Register a subcommand and add some standard CLI handling"""
 
     @functools.wraps(command)
-    def wrapped(
-        path_as_arg: Path | None, path: Path | None, *args, **kwargs
-    ) -> Callable:
-        if path_as_arg is not None and path is not None and path_as_arg != path:
-            raise SyntaxError("Conflicting values given for SAVE_PATH")
-        repo_root = path_as_arg or path or Path()
-        return command(repo_root, *args, **kwargs)
+    def wrapped(path: Path | None, *args, **kwargs) -> Callable:
+        return command((path or Path()).absolute(), *args, **kwargs)
 
-    wrapped = click.argument(
-        "path_as_arg",
-        type=Path,
-        required=False,
-        metavar="SAVE_PATH",
-    )(wrapped)
     wrapped = click.option(
         "--path",
         type=Path,
@@ -53,8 +43,44 @@ def _subcommand_init(command: Callable) -> Callable:
     type=str,
     help='Specify a description for this backup and "tag" it for future reference.',
 )
+@click.argument(
+    "path_as_arg",
+    type=Path,
+    required=False,
+    metavar="[SAVE_PATH]",
+)
 @_subcommand_init
-def backup(repo_root: Path, tag: str | None):
+def backup(repo_root: Path, path_as_arg: Path | None, tag: str | None):
     """Create a new backup"""
+    backup_.create_backup(path_as_arg or repo_root, tag)
 
-    backup_.create_backup(repo_root, tag)
+
+@click.option(
+    "--ignore",
+    type=str,
+    required=False,
+    multiple=True,
+    help=(
+        "Provide a glob pattern to ignore. Each ignore pattern"
+        ' must be prefaced with the "--ignore" flag.'
+    ),
+)
+@click.option(
+    "--track",
+    type=str,
+    required=False,
+    multiple=True,
+    help=(
+        "Provide a glob pattern to track (note: arguments without any flag will"
+        " also be treated as track patterns)."
+    ),
+)
+@click.argument(
+    "track_args", type=str, required=False, nargs=-1, metavar="[TRACK_PATTERN]..."
+)
+@_subcommand_init
+def init(
+    repo_root: Path, track_args: tuple[str], track: tuple[str], ignore: tuple[str]
+):
+    """Start tracking a save"""
+    onboard.create_repo(repo_root, *track_args, *track, ignore=ignore)
