9a0918351a43663f6ac898b5a1d5dbbb82cd14e5
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2025-05-02 14:32:41 -0400

    Implement hard rewind

--- gsb/rewind.py
+++ gsb/rewind.py
@@ -26,7 +26,9 @@ def generate_restore_tag_name(revision: str) -> str:
     return f"{backup.generate_tag_name()}.restore_of_{revision}"
 
 
-def restore_backup(repo_root: Path, revision: str, keep_gsb_files: bool = True) -> str:
+def restore_backup(
+    repo_root: Path, revision: str, keep_gsb_files: bool = True, hard: bool = False
+) -> str:
     """Rewind to a previous backup state and create a new backup
 
     Parameters
@@ -39,15 +41,26 @@ def restore_backup(repo_root: Path, revision: str, keep_gsb_files: bool = True) 
         By default, `.gsb_manifest` and `.gitignore` *will not* be restored
         (that is, the latest versions will be kept). To override this behavior,
         pass in `keep_gsb_files = False`.
+    hard : bool, optional
+        By default, any backups after the specified revision will be kept in
+        the history (with the specified restore point just copied to the top
+        of the stack). If you want to *fully erase* all progress after the
+        restore point (backed up or loose), pass in `hard = True` and then run
+        `git gc --aggressive --prune=now` afterward.
 
     Returns
     -------
     str
-        The tag name of the new restored backup
+        The name of the restored backup
 
     Notes
     -----
-    Before creating the backup, any un-backed up changes will first be backed up
+    - Before creating the backup, any un-backed up changes will first be backed up
+      unless `hard = True` is specified.
+    - When restoring with `hard = True`, **unsaved changes** to `.gsb_manifest`
+      and `.gitignore` will **not** be kept, even with `keep_gsb_files = True`.
+      Instead, the restored backup will contain the GSB files from the most
+      recent backup (tagged or otherwise).
 
     Raises
     ------
@@ -58,23 +71,31 @@ def restore_backup(repo_root: Path, revision: str, keep_gsb_files: bool = True) 
     """
     _git.show(repo_root, revision)
 
-    LOGGER.log(
-        IMPORTANT, "Backing up any unsaved changes before rewinding to %s", revision
-    )
-    try:
-        orig_head = backup.create_backup(
-            repo_root,
-            f"Backing up state before rewinding to {revision}",
+    orig_head = _git.show(repo_root, "HEAD").hash  # type: ignore[union-attr]
+
+    if not hard:
+        LOGGER.log(
+            IMPORTANT, "Backing up any unsaved changes before rewinding to %s", revision
         )
-    except ValueError:  #  nothing to back up
-        orig_head = _git.show(repo_root, "HEAD").hash  # type: ignore[union-attr]
+        try:
+            orig_head = backup.create_backup(
+                repo_root,
+                f"Backing up state before rewinding to {revision}",
+            )
+        except ValueError:  #  nothing to back up
+            pass
 
     _git.reset(repo_root, revision, hard=True)
-    _git.reset(repo_root, orig_head, hard=False)
     if keep_gsb_files:
         _git.checkout_files(repo_root, orig_head, backup.REQUIRED_FILES)
-    return backup.create_backup(
-        repo_root,
-        f"Restored to {revision}",
-        tag_name=generate_restore_tag_name(revision),
-    )
+    if not hard:
+        _git.reset(repo_root, orig_head, hard=False)
+
+    try:
+        return backup.create_backup(
+            repo_root,
+            f"Restored to {revision}",
+            tag_name=generate_restore_tag_name(revision),
+        )
+    except ValueError:
+        return revision
